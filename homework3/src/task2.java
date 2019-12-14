c2cbfd71bd31477db21b7c0d6a7da721b90e6f22
alexeykozovsky <akoz413@gmail.com>
2019-12-14 18:31:11 +0300

    Hello

---
+++ homework3/src/task2.java
@@ -0,0 +1,10 @@
+public class task2 {
+    public static void main(String[] args) {
+        String a = "Мама мыла раму";
+        String b = "Мама" + " мыла" + " раму";
+
+        System.out.println(a == b);
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
