590db388065214f9900bfe101542d307e8da37b0
alexeykozovsky <akoz413@gmail.com>
2020-01-17 01:09:51 +0300

    Exc

--- homework_17_01/src/task1/task1_2.java
+++ homework_17_01/src/task1/task1_2.java
@@ -3,8 +3,8 @@ package task1;
 public class task1_2 {
     public static void main(String[] args) {
         int z = 0;
-        // 1.2 try - finally
 
+        // 1.2 try - finally
         int i2 = 5;
         try {
             int x2 = i2 / z;
--- homework_17_01/src/task1/task1_3.java
+++ homework_17_01/src/task1/task1_3.java
@@ -2,8 +2,9 @@ package task1;
 
 public class task1_3 {
     public static void main(String[] args) {
-        // 1.3 try - catch - finally
         int z = 0;
+
+        // 1.3 try - catch - finally
         int i3 = 3;
         try {
             int x = i3 / z;
